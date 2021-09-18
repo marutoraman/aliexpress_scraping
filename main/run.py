804f03a8a0cb2d1848f3333e0cfec8bb925497cc
takashi002013@gmail.com <takashi002013@gmail.com>
2021-09-18 16:11:10 +0900

    1

--- main/run.py
+++ main/run.py
@@ -5,10 +5,6 @@ import sys
 import fire
 import pandas as pd
 
-# 環境変数(.env)の読み込み
-from dotenv import load_dotenv
-load_dotenv()
-
 # プロジェクトルートをPATHに追加
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 
@@ -22,30 +18,7 @@ logger = set_logger(__name__)
 
 
 def selenium(url :str, page_limit: int=5):
-    logger.info(f"url: {url} | page_limit: {page_limit}")
-    aliexpress = AliexpressScraping()
-    items = aliexpress.fetch_items(url, page_limit)
-
-    df = pd.DataFrame()
-    for item in items:
-        df = df.append(
-            item.to_dict(), ignore_index=True
-        )
-    df.to_csv(f"item_{now_timestamp()}.csv", mode="w", encoding="utf-8_sig")
-
-
-def requests(url :str, page_limit: int=5):
-    logger.info(f"url: {url} | page_limit: {page_limit}")
-    aliexpress = AliexpressRequests()
-    items = aliexpress.fetch_items(url, page_limit)
-
-    df = pd.DataFrame()
-    for item in items:
-        df = df.append(
-            item.to_dict(), ignore_index=True
-        )
-    df.to_csv(f"item_{now_timestamp()}.csv", mode="w", encoding="utf-8_sig")
-
+    pass
 
 if __name__ == "__main__":
-    fire.Fire()
+    fire.Fire(selenium)
--- readme.md
+++ readme.md
@@ -1,4 +1,4 @@
-Aliexpressスクレイピングツール
+Aliexpressスクレイピングツールの学習用
 ====
 
 # 使い方
@@ -7,5 +7,5 @@ python -m venv venv
 pip install -r requirements.txt
 . venv/bin/activate ※macの場合
 venv/scripts/activate ※windowsの場合
-python main/run.py selenium [収集したいurlを記述]
+
 ```
